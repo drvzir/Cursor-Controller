803a365bd12e352ab6dc10082f4a021865dceb95
Dhruv wazir <118794692+drvzir@users.noreply.github.com>
2024-09-14 12:13:10 +0530

    Delete pythonProject3/office_emp_proj directory

--- pythonProject3/office_emp_proj/emp_app/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Role , Employee,Department
-
-admin.site.register(Employee)
-admin.site.register(Role)
-admin.site.register(Department)
-
-
-# Register your models here.
